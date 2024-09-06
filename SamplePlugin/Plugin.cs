0377709a71a43dbdc05c6dd200216eab3bdc3742
cl-py <ivypool59@gmail.com>
2024-09-05 22:30:21 -0500

    testing for git

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -14,7 +14,7 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
+    private const string CommandName = "/test";
 
     public Configuration Configuration { get; init; }
 
